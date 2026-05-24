f6273c1f6fe2a811a60c3678c40af68aa967e7da
IbrahimBulle <abdiwaliibrahimbulle@gmail.com>
2026-05-24 14:53:19 +0300

    like url3

--- frontend/src/lib/config.js
+++ frontend/src/lib/config.js
@@ -1,4 +1,4 @@
-export const API_URL = ("https://restaurantbackend-w0jj.onrender.com" || "http://localhost:8080").replace(/\/$/, "");
+export const API_URL = ("https://restaurantbackend-w0jj.onrender.com" ).replace(/\/$/, "");
 export const WS_URL = ("wss://restaurantbackend-w0jj.onrender.com/ws" || API_URL.replace(/^http/, "ws") + "/ws").replace(/\/$/, "");
 
 export const ORDER_STATUS_FLOW = ["new", "accepted", "preparing", "ready", "served", "paid"];
